882c14dc72df15ee185fa3b32a45b79b5307815a
Alexander Hernández Herrera <aherrera@itcoint.com>
2021-03-22 18:18:21 -0600

    Agregar archivos de proyecto.

---
+++ vm-publish/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace vm_publish
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
